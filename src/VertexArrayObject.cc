f744900ca4b422ecfcdd4990d5a040f882759f88
listanegra <vitormac3@hotmail.com>
2021-06-09 21:41:33 -0300

    Use static cast over C-Style cast for compile time checks

--- src/VertexArrayObject.cc
+++ src/VertexArrayObject.cc
@@ -4,16 +4,18 @@ void VertexArrayObject::Add(const void *Data, GLint Length, GLint AttribSize) {
     this->Bind();
 
     GLuint BufferObject;
-    auto Index = (GLuint) Buffers.size();
+
+    auto Index = static_cast<GLuint>(Buffers.size());
+    auto SizeOfFloat = static_cast<GLint>(sizeof(GLfloat));
 
     glGenBuffers(1, &BufferObject);
     Buffers.push_back(BufferObject);
 
     glBindBuffer(GL_ARRAY_BUFFER, Buffers[Index]);
-    glBufferData(GL_ARRAY_BUFFER, Length * (GLint) sizeof(GLfloat), Data, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, Length * SizeOfFloat, Data, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(Index);
-    glVertexAttribPointer(Index, AttribSize, GL_FLOAT, GL_FALSE, AttribSize * (GLint) sizeof(GLfloat), nullptr);
+    glVertexAttribPointer(Index, AttribSize, GL_FLOAT, GL_FALSE, AttribSize * SizeOfFloat, nullptr);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     VertexArrayObject::Unbind();
