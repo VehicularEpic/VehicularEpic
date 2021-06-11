771ca4c515ee042bf59a46d38cb430b6711b56aa
listanegra <vitormac3@hotmail.com>
2021-06-10 22:03:16 -0300

    Added TailwindCSS and basic Vue page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    purge: [
+        './web/index.html',
+        './web/**/*.{vue,js,ts,jsx,tsx}'
+    ],
+    darkMode: false,
+    theme: {
+        extend: {}
+    },
+    variants: {
+        extend: {}
+    },
+    plugins: []
+}
---
+++ web/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container mx-auto"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App'
+});
+</script>
