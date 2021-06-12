73710dbb7e8fea4f950ae76151a3b6932916b4e7
listanegra <vitormac3@hotmail.com>
2021-06-12 11:06:58 -0300

    Vue path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { join } from 'path'
+import { join, resolve } from 'path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -10,5 +10,10 @@ export default defineConfig({
     publicDir: join(__dirname, 'web', 'public'),
     build: {
         outDir: join(__dirname, 'dist')
+    },
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, '/src')
+        }
     }
 });
