5c7f39e135c22e2729949343fff76e09e389fb46
listanegra <vitormac3@hotmail.com>
2021-06-12 11:14:17 -0300

    Basic button component

---
+++ web/src/components/Button.vue
@@ -0,0 +1,25 @@
+<template>
+    <button
+        @click.stop="click"
+        class="hover:bg-opacity-20 bg-opacity-10 rounded focus:outline-none w-full h-8"
+    >
+        <div class="backdrop-filter backdrop-blur">
+            <span class="text-white uppercase">
+                <slot></slot>
+            </span>
+        </div>
+    </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'Button',
+    methods: {
+        click(e: Event) {
+            this.$emit('click', e);
+        }
+    }
+});
+</script>
