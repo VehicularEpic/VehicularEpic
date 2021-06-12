03873e9151160b258b25829b2c13e343e45e0da1
listanegra <vitormac3@hotmail.com>
2021-06-12 11:45:34 -0300

    Unnecessary call to click event

--- web/src/components/Button.vue
+++ web/src/components/Button.vue
@@ -1,8 +1,5 @@
 <template>
-    <button
-        @click.stop="click"
-        class="hover:bg-opacity-20 bg-opacity-10 rounded focus:outline-none w-full h-8"
-    >
+    <button class="hover:bg-opacity-20 bg-opacity-10 rounded focus:outline-none w-full h-8">
         <div class="backdrop-filter backdrop-blur">
             <span class="text-white uppercase">
                 <slot></slot>
@@ -16,10 +13,5 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
     name: 'Button',
-    methods: {
-        click(e: Event) {
-            this.$emit('click', e);
-        }
-    }
 });
 </script>
