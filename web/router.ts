c1db184bc46f1ec2e873afe637233a224fd3edb5
listanegra <vitormac3@hotmail.com>
2021-06-12 11:13:02 -0300

    Added vue-router

---
+++ web/router.ts
@@ -0,0 +1,9 @@
+import VueRouter, { createRouter, createWebHistory } from 'vue-router'
+
+
+const routes: VueRouter.RouteRecordRaw[] = [
+
+];
+
+const router = createRouter({ history: createWebHistory(), routes });
+export default router;
